10c8b06a46004eb6028bbad01e85c380f4069ec7
Travis CI <yadusurya666@gmail.com>
2026-04-20 22:09:57 +0530

    Design hero changed

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -1,12 +1,12 @@
 export const site : any = {
     name : 'mag.shoppy',
     instagram:'mag.shoppy',
-    phone:"9495314108",
+    phone:"9656160947",
     google_map:"",
     email:"fashionshoes110@gmail.com",
     address:"India (based in kerala)",
     social : {
         instagram:'mag.shoppy',
-        whatsapp:'9495314108'
+        whatsapp:'9656160947'
     }
 }
